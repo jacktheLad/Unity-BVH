f6afd86d453e52aef2994c6b05a99eccf708199a
jacktheLad <461505320@qq.com>
2020-03-02 23:55:25 +0800

    initial commit

---
+++ Assets/BVH/BVHCommon.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+namespace sif
+{
+    using BVHTriangle = Vector3Int;
+    public class BVHScene
+    {
+        List<BVHTriangle> _triangles;
+        List<Vector3> _vertices;
+
+        public List<BVHTriangle> Triangles { get => _triangles; }
+        public List<Vector3> Vertices { get => _vertices; }
+
+        public BVHScene(List<BVHTriangle> tris, List<Vector3> verts)
+        {
+            _triangles = tris;
+            _vertices = verts;
+        }
+
+    }
+}
---
+++ Assets/BVH/SBVH/CPU/CPU_SBVHBuilder.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace sif
+{
+    public class CPU_SBVHBuilder
+    {
+        const float SPLIT_ALPHA = 1.0E-5F;
+
+        private CPU_SBVHData _bvh;
+        private CPU_SBVHBuilder(CPU_SBVHData bvh)
+        {
+            _bvh = bvh;
+        }
+
+        public static void Build(CPU_SBVHData bvh)
+        {
+            var builder = new CPU_SBVHBuilder(bvh);
+        }
+
+    }
+}
+
