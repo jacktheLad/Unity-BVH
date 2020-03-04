4321e6bfe586d201281774be833cb5aeada3e599
jacktheLad <461505320@qq.com>
2020-03-04 22:15:03 +0800

    NothingToSay

--- Assets/BVH/SBVH/CPU/CPU_SBVHBuilder.cs
+++ Assets/BVH/SBVH/CPU/CPU_SBVHBuilder.cs
@@ -6,9 +6,7 @@ namespace sif
 
     public class CPU_SBVHBuilder
     {
-        const float SPLIT_ALPHA = 1.0E-5F;
-
-        // 写这么长是因为C#里面值类型（struct）指定默认初始化值没有更好的方法了。
+        // 写这么长是因为C#里面值类型（struct）指定默认(非0)初始化值没有更好的方法了。
         struct PrimitiveRef
         {
             private int _triangleIdx;
@@ -23,19 +21,91 @@ namespace sif
                 _bounds = bounds;
             }
 
-            public static PrimitiveRef New() {
-                return new PrimitiveRef(-1, AABB.New());
-            }
-
-            public static PrimitiveRef New(int triIdx, AABB bounds)
+            public static PrimitiveRef New() 
             {
-                return new PrimitiveRef(triIdx, bounds);
+                return new PrimitiveRef(-1, AABB.New());
             }
         }
 
+        /// <summary>
+        /// 一个简化的Node，不包含子Node
+        /// </summary>
+        struct NodeSpec
+        {
+            public int NumRef { get; set; }
+            public AABB Bounds { get; set; }
+        }
          
+        struct ObjectSplit
+        {
+            public float SAH { get; set; }
+            public byte Dim { get; set; }
+            public int NumLeftRef { get; set; }
+            public AABB LeftBounds { get; set; }
+            public AABB RightBounds { get; set; }
+            private ObjectSplit(int nothing)
+            {
+                SAH = float.MaxValue;
+                Dim = 0;
+                NumLeftRef = 0;
+                LeftBounds = AABB.New();
+                RightBounds = AABB.New();
+            }
+
+            public static ObjectSplit New()
+            {
+                return new ObjectSplit(0);
+            }
+        }
+
+        struct SpatialSplit
+        {
+            public float SAH { get; set; }
+            public byte Dim { get; set; }
+            public float Pos { get; set; }
+            private SpatialSplit(int nothing)
+            {
+                SAH = float.MaxValue;
+                Dim = 0;
+                Pos = 0f;
+            }
+            public static SpatialSplit New()
+            {
+                return new SpatialSplit(0);
+            }
+        }
+
+        struct SpatialBin
+        {
+            public AABB Bounds { get; set; }
+            public int Enter { get; set; }
+            public int Exit { get; set; }
+
+            private SpatialBin(int nothing)
+            {
+                Bounds = AABB.New();
+                Enter = 0;
+                Exit = 0;
+            }
+            public static SpatialBin New()
+            {
+                return new SpatialBin(0);
+            }
+        }
+
+        private const float SPLIT_ALPHA = 1.0E-5F;
+        private const int N_SPATIAL_BINS = 32;
+        private const int MAX_DEPTH = 64;
+        private const int MAX_SPATIAL_DEPTH = 48;
 
         private CPU_SBVHData _bvhData;
+        private List<PrimitiveRef> _refStack; // A linear stack.
+        private float _totalArea;
+        private List<AABB> _rightBounds;
+        private byte _sortDim;
+        private SpatialBin[,] _bins = new SpatialBin[3, N_SPATIAL_BINS];
+        private int _numDuplicates;
+
         private CPU_SBVHBuilder(CPU_SBVHData bvhData)
         {
             _bvhData = bvhData;
@@ -44,8 +114,6 @@ namespace sif
         public static void Build(CPU_SBVHData bvhData)
         {
             var builder = new CPU_SBVHBuilder(bvhData);
-
-
         }
 
     }
---
+++ _fast_commit.bat
@@ -0,0 +1,4 @@
+git add .
+git commit -a -m "NothingToSay"
+git push origin master
+pause
